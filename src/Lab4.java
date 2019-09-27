fa8c80a5616cadb2001f0a34f3e36e3e38dd9879
Turk Erdin <ufkun@erdin.org>
2019-09-27 12:25:48 -0700

    Final upload of in-class activity

---
+++ src/Lab4.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+/**
+ * Directions: I've provided the essentials of what you need to make this work. If you don't want to follow along in class
+ * and want to do it on your own, go through all the "to do's" and and implement the requirements. Submit to iLearn before
+ * 3pm on lab day for full credit on the lab. NOTE: You will still need to show up for attendance to get your attendance
+ * points.
+ */
+
+public class Lab4 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); // In future exercises, I won't provide this - you guys will be responsible for initializing your own scanners
+        // TODO: Take 2 int inputs from the user. Set A and B using our setter methods.
+
+        in.nextLine(); // This is to clear the new line carriage from the scanner buffer
+
+        // TODO: Ask the user which operation (^,*,+,-,/) they wish to perform. Then, use a switch statement to call the proper method
+        System.out.println("Which operation would you like to perform? (^,*,+,-,/)");
+
+
+    }
+
+    // TODO: We want methods for add(), subtract(), multiply(), divide(), power()
+
+    public static void helloWorld(String name) {
+        // TODO: Create a function that prints out "Hello World, I am <name>!"
+    }
+
+
+}
